de314c16678d4930ff15248d12cc079eff892c41
Hectorgamecreator <rodrh308@newschool.edu>
2022-12-04 00:58:39 -0500

    Adding crafting system scripts

---
+++ Tenochtitlan/Assets/Scripts/Item.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+[CreateAssetMenu(fileName = "Item", menuName = "Item/baseItem")]
+
+public class Item : ScriptableObject
+{
+    new public string name = "Default Item";
+    public Sprite icon = null;
+    public string itemDescription = "Used for crafting";
+
+
+    public virtual void Use()
+    {
+        Debug.Log("Using" + name);
+    }
+
+    public virtual string GetItemDescription()
+    {
+        return itemDescription;
+    }
+}
+
--- Tenochtitlan/Assets/Scripts/PlayerController.cs
+++ Tenochtitlan/Assets/Scripts/PlayerController.cs
@@ -10,19 +10,39 @@ namespace HectorRodriguez
         [SerializeField]
         private float CharacterSpeed = 1.0f;
         private Vector2 InputDir;
+       
 
         public Rigidbody2D PlayerPhysics;
 
+
+        //adding this
+        [SerializeField]
+        private InventoryUi inventoryUi;
+
+
+       
         // collect references
-        void Awake()
+       private void Awake()
         {
 
             PlayerPhysics = GetComponent<Rigidbody2D>();
 
+
+        }
+        private void Start()
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
         }
 
-        void Update()
+
+
+        private void Update()
         {
+
+            if (!inventoryUi.InventoryOpen)
+            {
+            }
 
             float VertInput = Input.GetAxis("Vertical");
             float HorInput = Input.GetAxis("Horizontal");
